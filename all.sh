814d099bca900c752a3bc7962ccd726819fce20d
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-05-03 10:52:23 +0700

    scripts: all: remove previous script before start build
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- all.sh
+++ all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rm -rf resukisu.sh
 wget https://raw.githubusercontent.com/nekoshirro/platform_manifest-marble/refs/heads/kernel/resukisu.sh
 chmod +x resukisu.sh
 ./resukisu.sh
+rm -rf resukisu.sh
 
+rm -rf kernelsu-next.sh
 wget https://raw.githubusercontent.com/nekoshirro/platform_manifest-marble/refs/heads/kernel/kernelsu-next.sh
 chmod +x kernelsu-next.sh
 ./kernelsu-next.sh
+rm -rf kernelsu-next.sh
