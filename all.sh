d6d689ea0792bc40d50921a3ce6213deada30c6c
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-06-14 15:29:58 +0700

    scripts: add build script for non-root variant
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- all.sh
+++ all.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Non-root Kernel
+rm -rf non-root.sh
+wget https://raw.githubusercontent.com/nekoshirro/platform_manifest-marble/refs/heads/kernel/non-root.sh
+chmod +x non-root.sh
+./non-root.sh
+rm -rf non-root.sh
+
+# ReSukiSU Kernel
 rm -rf resukisu.sh
 wget https://raw.githubusercontent.com/nekoshirro/platform_manifest-marble/refs/heads/kernel/resukisu.sh
 chmod +x resukisu.sh
 ./resukisu.sh
 rm -rf resukisu.sh
 
+# KernelSU-Next kernel
 rm -rf kernelsu-next.sh
 wget https://raw.githubusercontent.com/nekoshirro/platform_manifest-marble/refs/heads/kernel/kernelsu-next.sh
 chmod +x kernelsu-next.sh
