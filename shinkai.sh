9d0127f771030cd68a98b159f4d47a6c67a0480a
Hafidz Muzakky <ais.muzakky@gmail.com>
2026-09-01 03:45:25 +0000

    scripts: Track lineage-24.0 branch of MiuiCamera from fiqri
    Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

--- shinkai.sh
+++ shinkai.sh
@@ -167,9 +167,9 @@ start_build_process() {
     echo "Cloning device stuff..."
     # Device Trees
     git clone https://github.com/nekoshirro/platform_device_xiaomi_marble.git device/xiaomi/marble -b shinkai-17 --depth 1
-    git clone https://github.com/fiqri19102002/android_device_xiaomi_miuicamera-marble.git -b lineage-23.2 device/xiaomi/miuicamera-marble
+    git clone https://github.com/fiqri19102002/android_device_xiaomi_miuicamera-marble.git -b lineage-24.0 device/xiaomi/miuicamera-marble
     git clone https://github.com/nekoshirro/platform_vendor_xiaomi_marble.git -b 17 vendor/xiaomi/marble
-    git clone https://codeberg.org/fiqri19102002/proprietary_vendor_xiaomi_miuicamera-marble.git -b lineage-23.2 vendor/xiaomi/miuicamera-marble
+    git clone https://codeberg.org/fiqri19102002/proprietary_vendor_xiaomi_miuicamera-marble.git -b lineage-24.0 vendor/xiaomi/miuicamera-marble
     git clone --recurse-submodules https://github.com/nekoshirro/platform_kernel_xiaomi_marble.git -b 17 kernel/xiaomi/marble --depth 1
     git clone https://github.com/nekoshirro/platform_kernel_xiaomi_marble-devicetrees.git kernel/xiaomi/marble-devicetrees --depth 1
     git clone https://github.com/nekoshirro/platform_kernel_xiaomi_marble-modules.git kernel/xiaomi/marble-modules --depth 1
